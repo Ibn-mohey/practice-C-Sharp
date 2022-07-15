98f6124cdda4309013c5a37868ead71e42ba11ed
ibn-mohey-apps <asd_vas@yahoo.com>
2022-07-15 10:27:49 +0200

    study

---
+++ revision/revision/01.Basics for Beginners/4. Non-Primitive Types.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revision._01.Basics_for_Beginners
+{
+    internal class person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("my name is " + FirstName + " " + LastName);
+        }
+    }
+
+    internal class StringClass
+    {
+        public static void staticStringMethod()
+        {
+            var firstname = "abdo";
+
+            var lastname = "mohey";
+
+            var name = string.Format("{0} {1}", firstname, lastname);
+
+            Console.WriteLine(name);
+            var names = new string[3] { "ahmed", "mohamed", "abdo" };
+            var fromlist = string.Join(" separetor ", names);
+            Console.WriteLine(fromlist);
+
+            var bigText = @"this is
+well formated text
+to location
+c:\folder1\forlder2";
+            Console.WriteLine(bigText);
+        }
+    }
+
+    internal class EnumClass
+    {
+        public enum ShippingMethods
+        {
+            meth1 = 1,
+            meth2 = 2,
+            meth3 = 3
+        }
+
+        public static void main()
+        {
+            var meth = ShippingMethods.meth1;
+            Console.WriteLine((int)meth);
+
+            var methID = 3;
+            Console.WriteLine((ShippingMethods)methID);
+            var method4 = "meth4";
+            var ShippingMethod = (ShippingMethods)Enum.Parse(typeof(ShippingMethods), method4);
+        }
+    }
+}
---
+++ revision/revision/Program.cs
@@ -0,0 +1,39 @@
+﻿using revision._01.Basics_for_Beginners;
+using System;
+
+namespace revision
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("hello world\n------\n");
+
+            // get to the neme space
+            //testclass.method();
+
+            //// lesson 4
+            //var jhon = new person();
+            //jhon.FirstName = "jhon";
+            //jhon.LastName = "smith";
+            //jhon.Introduce();
+
+            //StringClass.staticStringMethod();
+            //EnumClass.main();
+
+            /// leson 5
+            ///
+            //ConditionalStatements.main(false);
+            //ConditionalStatements.demo2();
+            //Console.WriteLine("---\n");
+            //iterationStatments.iterationdemo();
+
+            //RandomClass.demo();
+
+            //listAndArrays.demo2();
+
+            workingWithData.demo();
+            Console.WriteLine("\n--------\nend of the program\n");
+        }
+    }
+}
