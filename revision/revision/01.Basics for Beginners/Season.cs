2c894820b795ea5c2c382e6e077de164d5cbd49f
ibn-mohey-apps <asd_vas@yahoo.com>
2022-07-15 10:28:17 +0200

    study time

---
+++ revision/revision/01.Basics for Beginners/Season.cs
@@ -0,0 +1,11 @@
+﻿namespace revision._01.Basics_for_Beginners
+{
+
+        public enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+}
---
+++ revision/revision/01.Basics for Beginners/working with data, text ,files.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revision._01.Basics_for_Beginners
+{
+    internal class workingWithData
+    {
+        public static void demo()
+        {
+            var datetime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            Console.WriteLine(now.Hour + ":" + now.Minute);
+        }
+    }
+}
